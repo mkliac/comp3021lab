f473b08c5fd016b47f8028db4a0b744988c76011
MingKei Li <mkliac@connect.ust.hk>
2022-03-14 13:10:03 +0800

    lab4 commit

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -3,9 +3,14 @@ package base;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class NoteBook {
+public class NoteBook implements java.io.Serializable{
 	private ArrayList<Folder> folders;
+	private static final long serialVersionUID = 1L;
 	
 	public NoteBook() {
 		folders = new ArrayList<Folder>();
@@ -64,5 +69,36 @@ public class NoteBook {
 	public boolean createTextNote(String folderName, String title, String content) {
 		TextNote note = new TextNote(title, content);
 		return insertNote(folderName, note);
+	}
+	
+	public boolean save(String file) {
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(file);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(this);
+			out.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public NoteBook(String file) {
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		try {
+			fis = new FileInputStream(file);
+			in = new ObjectInputStream(fis);
+			NoteBook object = (NoteBook)in.readObject();
+			in.close();
+			folders = new ArrayList<Folder>();
+			for(int i = 0; i < object.folders.size(); i++) {
+				folders.add(object.folders.get(i));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,6 +1,15 @@
 package base;
 
-public class TextNote extends Note{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class TextNote extends Note implements java.io.Serializable{
 	public String content;
 	public TextNote(String title) {
 		super(title);
@@ -9,5 +18,41 @@ public class TextNote extends Note{
 	public TextNote(String title, String content) {
 		super(title);
 		this.content = content;
+	}
+	
+	public TextNote(File f) {
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+	}
+	
+	private String getTextFromFile(String absolutePath) {
+		String result = "";
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(absolutePath)));
+			String line = null;
+			while((line = br.readLine()) != null) {
+				result += line;
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public void exportTextToFile(String pathFolder) {
+		if(pathFolder == "") {
+			pathFolder = ".";
+		}
+		File file = new File(pathFolder + File.separator + this.getTitle().replaceAll(" ", "_") + ".txt");
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			bw.write(content);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
